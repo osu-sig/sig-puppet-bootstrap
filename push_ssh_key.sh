56f527a51e371a2fc8e8cc779bca11072dbe698d
Stacy Brock <brock@oregonstate.edu>
2014-08-27 14:07:54 -0700

    Add some validation and return proper exit status
    - validates existence of GITHUB_TOKEN environment variable
    - checks the http status code of the GitHub API request and
      returns a proper exit status code

--- push_ssh_key.sh
+++ push_ssh_key.sh
@@ -2,4 +2,16 @@
 
 PUBLIC_KEY=`cat ~/.ssh/id_rsa.pub`
 
-curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/keys -d "{ \"key\": \"$PUBLIC_KEY\" }"
+: ${GITHUB_TOKEN:?"Environment variable GITHUB_TOKEN must be present."}
+
+response=$(curl -w \\n%{http_code} --silent -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/user/keys -d "{ \"key\": \"$PUBLIC_KEY\" }")
+statuscode=$(grep -Po '\s(\d+)$' <<< $response | sed 's/\s//')
+message=$(grep -Po '"message":.*?[^\\]",' <<< $response)
+
+if [[ $statuscode -ne '201' ]]
+then
+  echo "status code: $statuscode $message"
+  exit 1
+else
+  exit 0
+fi
